46a951bc17a034ad5b6bcf09495c916b8c69509f
Yuaraj <68475998+Yuaraj@users.noreply.github.com>
2021-04-23 12:45:22 +0530

    Addition of Digit

--- additionDigit.cpp
+++ additionDigit.cpp
@@ -1,12 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
+ 
+int addDigit(long int n)
+{
+    int sum = 0;
+    while(n > 0 || sum > 9)
+    {
+        if(n == 0)
+        {
+            n = sum;
+            sum = 0;
+        }
+        sum += n % 10;
+        n /= 10;
+    }
+    return sum;
+}
 
-int main(){
-	cout <<"Input:";
-	ios::sync_with_stdio(false);
-	cin.tie(0);
-	int n;
-	cin >> n;
-	cout << "Output:" << (n%9) << '\n';
-	return 0;
+int main()
+{
+    long int n;
+    cout << "Enter A Number:";
+    cin >> n;
+    cout << addDigit(n);
+    return 0;
 }
