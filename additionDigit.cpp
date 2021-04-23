f5b2408660b236e316198a971e2c4d4ecd4ca6e0
Yuaraj <68475998+Yuaraj@users.noreply.github.com>
2021-04-23 12:37:18 +0530

    Addition of Digit

---
+++ additionDigit.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	cout <<"Input:";
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int n;
+	cin >> n;
+	cout << "Output:" << (n%9) << '\n';
+	return 0;
+}
